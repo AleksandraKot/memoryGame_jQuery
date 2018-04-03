3d3b935a024512dffc6ad3254348626a4e4aa501
Aleksandra <pyzakot@gmail.com>
2018-04-03 12:12:36 +0200

    Start function added, no restart game yet

--- js/app.js
+++ js/app.js
@@ -31,22 +31,21 @@ $(document).ready(function () {
 
         return arrayOfCardsInRandomOrder;
     }
-    reorderedCardsArray = shuffle(createArrayOfCardsPair(arrayOfImages));
-    console.log(reorderedCardsArray);
+
 
     // Creating gameboard of reversed cards
 
     function createGameBoardOfFlippedCards(array) {
         for (var i = 0; i < array.length; i++) {
             var card =  $("<div>");
-            card.addClass('card');
+            card.addClass('size');
             card.data('image', array[i]);
             console.log(card.data());
             card.addClass('covered');
             gameContainer.append(card);
         }
     }
-    createGameBoardOfFlippedCards(reorderedCardsArray);
+
 
     // Turning cards to front on click
 
@@ -66,24 +65,43 @@ $(document).ready(function () {
                 alreadyFlipedCards = $('.flipping-animation');
                 if (alreadyFlipedCards.length === 2) {
                     hideIfTheSamePicture(alreadyFlipedCards);
-                    // continueIfDifferent(alreadyFlipedCards);
+                    continueIfDifferent(alreadyFlipedCards);
                 }
             });
         }
         function hideIfTheSamePicture(elements) {
-            if (elements.first().data('image') === elements.last().data('image')) {
+            if (elements.eq(0).data('image') === elements.eq(1).data('image')) {
                 var timeout1 = setTimeout(function () {
-                    elements.first().addClass("hidden");
-                    elements.last().addClass("hidden");
-                    elements.first().removeClass("flipping-animation");
-                    elements.last().removeClass("flipping-animation");
+                    elements.eq(0).addClass("hidden");
+                    elements.eq(1).addClass("hidden");
+                    elements.eq(0).removeClass("flipping-animation");
+                    elements.eq(1).removeClass("flipping-animation");
                 }, 500);
             }
         }
+        function continueIfDifferent(elements) {
+                if (elements.eq(0).data('image') !== elements.eq(1).data('image')) {
     
-    turnCardsToFront( $('.covered'));
+                    var timeout1 = setTimeout(function() {
+                        elements.eq(0).toggleClass("flipping-animation");
+                        elements.eq(0).addClass("covered");
+                        elements.eq(1).toggleClass("flipping-animation");
+                        elements.eq(1).addClass("covered");
+                        elements.eq(0).removeClass("flipping-animation");
+                        elements.eq(1).removeClass("flipping-animation");
+                    }, 500);
+                }
+            }
+    
 
+    function startGame() {
+        reorderedCardsArray = shuffle(createArrayOfCardsPair(arrayOfImages));
+        createGameBoardOfFlippedCards(reorderedCardsArray);
+        coveredCards = $('.covered');
+        turnCardsToFront(coveredCards);
+    }
 
+    startGame();
 
 
 
