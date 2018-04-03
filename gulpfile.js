d858eb01775d082a0581c492d5912b45b440df5a
Aleksandra <pyzakot@gmail.com>
2018-04-03 09:37:20 +0200

    Gameboard with fliped cards ready, basic flex layout added

--- gulpfile.js
+++ gulpfile.js
@@ -1,32 +1,3 @@
-//  var gulp = require('gulp');
-//  var sass = require('gulp-sass');
-//  var sourcemaps = require("gulp-sourcemaps");
-//  var browsersync = require("browser-sync").create();
-
-//  gulp.task('sass', function() {
-//    return gulp.src('scss/*.scss')
-//    .pipe(sourcemaps.init())
-//    .pipe(sass({outputStyle: 'expanded', errLogToConsole: true}).on('error', sass.logError))
-//    .pipe(sourcemaps.write('./maps')) 
-//    .pipe(gulp.dest('css'))
-// });
-
-// gulp.task('watch', function(){
-// gulp.watch('scss/**/*.scss', ['sass']);
-// })
-// gulp.task('browsersync', function() {
-//    var files = [
-//       '*.html',
-//       'css/**.css',
-//       'js/**js'
-//    ];
-//    browsersync.init(files, {
-//       server: {
-//          baseDir: './'
-//       }
-//    });
-// });
-// gulp.task("default", ["watch", "browsersync"]);
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
--- js/app.js
+++ js/app.js
@@ -1,5 +1,63 @@
-$(document).ready(function() {
+$(document).ready(function () {
+
+    var gameContainer = $('.game-container').first();
+    var arrayOfImages = ["blue", "green", "pink", "purple", "purplelight"];
+    var reorderedCardsArray;
+    var coveredCards;
+
+    // Multiplying elements in array in order to get pair of cards
+
+    function createArrayOfCardsPair(array) {
+
+        var multipliedArrayOfCards = [];
+
+        $.each(arrayOfImages, function (index, value) {
+            for (var i = 0; i < 2; i++) {
+                multipliedArrayOfCards.push(value);
+            }
+
+        });
+
+        console.log(multipliedArrayOfCards);
+        return multipliedArrayOfCards;
+
+    }
+    // Reaordering elements of array
+
+    function shuffle(array) {
+        var arrayOfCardsInRandomOrder = array.sort(function (a, b) {
+            return 0.5 - Math.random();
+        });
+
+        return arrayOfCardsInRandomOrder;
+    }
+    reorderedCardsArray = shuffle(createArrayOfCardsPair(arrayOfImages));
+    console.log(reorderedCardsArray);
+
+    // Creating gameboard of reversed cards
+
+    function createGameBoardOfFlippedCards(array) {
+        for (var i = 0; i < array.length; i++) {
+            var card =  $("<div>");
+            card.addClass('card');
+            card.data('image', array[i]);
+            card.addClass('covered');
+            gameContainer.append(card);
+        }
+    }
+    createGameBoardOfFlippedCards(reorderedCardsArray);
 
 
-    
+
+
+
+
+
+
+
+
+
+
+
+
 });
